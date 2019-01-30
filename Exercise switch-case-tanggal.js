4453abb3164ec1e2eaf3b304762435ed9cce0365
Wahyu Pambudi <wahyupambudi823@gmail.com>
2019-01-30 13:46:57 +0700

    wahyupambudi

--- Exercise switch-case-tanggal.js
+++ Exercise switch-case-tanggal.js
@@ -1,47 +1,118 @@
-var tanggal = '7'
-var bulan = '12'
-var tahun = '2019'
+var tanggal = 21;
+var bulan = 2;
+var tahun = 2019;
 
 
 switch(bulan){
-    case '1':
-        console.log(tanggal+' Januari ' + tahun)
+    case 1:
+    bulan = 'Januari'
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal+' Januari ' + tahun)
         break;
-    case '2':
-        console.log(tanggal+' Febuari ' + tahun)
+    case 2:
+    bulan = 'Febuari';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal+' Febuari ' + tahun)
         break;
-    case '3':
-        console.log(tanggal + ' Maret ' + tahun)
+    case 3:
+    bulan = 'Maret';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+    // console.log(tanggal + ' Maret ' + tahun)
         break;
-    case '4':
-        console.log(tanggal + ' April ' + tahun)
+    case 4:
+    bulan = 'April';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+    // console.log(tanggal + ' April ' + tahun)
         break;    
-    case '5':
-        console.log(tanggal + ' Mei ' + tahun)
+    case 5:
+    bulan = 'Mei';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' Mei ' + tahun)
         break;
-    case '6':
-        console.log(tanggal+' Juni ' + tahun)
+    case 6:
+    bulan = 'Juni';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal+' Juni ' + tahun)
         break;
-    case '7':
-        console.log(tanggal+' Juli ' + tahun)
+    case 7:
+    bulan = 'Juli';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal+' Juli ' + tahun)
         break;
-    case '8':
-        console.log(tanggal + ' Agustus ' + tahun)
+    case 8:
+    bulan = 'Agustus';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' Agustus ' + tahun)
         break;
-    case '9':
-        console.log(tanggal + ' September ' + tahun)
+    case 9:
+    bulan = 'September';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' September ' + tahun)
         break;    
-    case '10':
-        console.log(tanggal + ' Oktober ' + tahun)
+    case 10:
+    bulan = 'Oktober';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' Oktober ' + tahun)
         break;
-    case '11':
-        console.log(tanggal + ' November ' + tahun)
+    case 11:
+    bulan = 'November';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' November ' + tahun)
         break;    
-    case '12':
-        console.log(tanggal + ' Desember ' + tahun)
+    case 12:
+    bulan = 'Desember';
+    if ( tanggal <= 31 && tahun <= 2200 ) {
+        console.log ( tanggal +' '+ bulan +' '+ tahun )
+    }else{
+        console.log('Tanggal / Bulan / Tahun Melampaui Batas')
+    }
+        // console.log(tanggal + ' Desember ' + tahun)
         break;
-
     default:
-    console.log('Tidak ada Bulan '+bulan)
-
+    console.log('bulan tidak ditemukan')
 }
+// console.log(bulan)
